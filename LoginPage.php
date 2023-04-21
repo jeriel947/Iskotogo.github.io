2f487b94e4f16c55a22bd2ee6106cdd9cbeeac74
jeriel947 <71412518+jeriel947@users.noreply.github.com>
2023-04-22 02:17:43 +0800

    Add files via upload

---
+++ LoginPage.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <?php session_start(); ?>
+
+    <?php 
+        session_start();
+        
+        $con = mysqli_connect('localhost', 'root', '','submissions');
+        $sql = "SELECT * from tb_submissions";
+        $res = mysqli_query($con, $sql);
+        $photoID=array();
+        $photoNames=array();
+        $userNames=array();
+        
+        if ($res->num_rows > 0) {
+            // output data of each row
+            while($row = $res->fetch_assoc()) {
+                array_push($photoID, $row['submission_id']);
+                array_push($photoNames, $row['photoname']);
+                array_push($userNames, $row['user']);
+            }
+        } else {
+            echo "0 results";
+        }
+    ?>
+
+    <title>LOGIN</title>
+
+    <link rel="stylesheet" type="text/css" href="loginStyle.css">
+
+</head>
+
+<body>
+
+    
+    <form action="login.php" method="post" id="login-box">
+
+        <?php if (isset($_GET['error'])) { ?>
+
+            <p class="error"><?php echo $_GET['error']; ?></p>
+
+        <?php } ?>
+        
+        <div id="left">
+            <img src="images/logo.png" id="logo">
+
+            <div id="input">
+
+                <input type="text" name="user_name" placeholder="Enter your student Number / username"><br>
+
+                <input type="password" name="password" placeholder="Password"><br> 
+
+                <button type="submit">Login</button>
+            </div>
+        </div>
+
+        <div id="right">
+            <img src="images/burger.png">
+        </div>
+
+    </form>
+
+</body>
+
+</html>
