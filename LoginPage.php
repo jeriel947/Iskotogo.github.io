da0c2862693de01cf53f249879abacff0b8585d1
jeriel947 <jerielledesma@gmail.com>
2023-05-05 19:11:56 +0800

    database connection update 2
    fixed password

--- LoginPage.php
+++ LoginPage.php
@@ -8,7 +8,7 @@
     <?php 
         session_start();
         
-        $con = mysqli_connect('localhost', 'iskotogo', '1357','submissions');
+        $con = mysqli_connect('localhost', 'iskotogo', '13579','submissions');
         $sql = "SELECT * from tb_submissions";
         $res = mysqli_query($con, $sql);
         $photoID=array();
