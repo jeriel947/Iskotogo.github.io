c69c03868a87a40027a397a069d917c6eaf413bf
jeriel947 <jerielledesma@gmail.com>
2023-05-19 23:56:09 +0800

    profile tab test

--- PROFILE.php
+++ PROFILE.php
@@ -535,6 +535,24 @@
                 counter = 1;
             }
         }, 5000);
+
+        /* SHOW PROFILE */
+        const openProfile = document.querySelector('#profile');
+        const closeProfile = document.querySelector('.close-profile');
+        const popUpProfile = document.querySelector('.show-profile');
+        const profileContainer = document.querySelector('.profile-container');
+
+        openProfile.addEventListener('click', () => {
+            popUpProfile.style.visibility = "visible";
+            popUpProfile.style.opacity = "1";
+            profileContainer.style.transform = profileContainer.style.transform === 'translateX(0%)' ? 'translateX(100%)' : 'translateX(0%)';
+        });
+
+        closeProfile.addEventListener('click', () => {
+            popUpProfile.style.visibility = "hidden";
+            popUpProfile.style.opacity = "0";
+            profileContainer.style.transform = profileContainer.style.transform === 'translateX(0%)' ? 'translateX(100%)' : 'translateX(0%)';
+        });
     </script>
 
     <!-- Swiper JS -->
