bf4f920ddf058ebb92aa91af65089d8d3410742d
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2023-07-08 12:48:35 +0800

    Update vendor's page

--- components/vendor/dashboard.php
+++ components/vendor/dashboard.php
@@ -3,20 +3,20 @@
         <div class="border">
         <div class="views">
             <p class="label">Views</p>
-            <p class="top_highlight"><i class="bi bi-eye"></i>&nbsp;11,143</p>
+            <p class="top_highlight"><i class="bi bi-eye"></i>&nbsp;-</p>
         </div>
         </div>
         <div class="border">
         <div class="hearts">
             <p class="label">Hearts</p>
-            <p class="top_highlight"><i class="bi bi-heart"></i>&nbsp;1,669</p>
+            <p class="top_highlight"><i class="bi bi-heart"></i>&nbsp;-</p>
         </div>
         </div>
     </div>
     <div class="border">
         <div class="bottom">
         <p class="label">Today's Sales</p>
-        <p id="value">P 2,576.00</p>
+        <p id="value">-</p>
         <button type="button" class="view-details-btn btn">View Details</button>
         </div>
     </div>
