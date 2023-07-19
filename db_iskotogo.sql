79ada3ffbb485f8b423242127b3d0cd9f7e010fa
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2023-07-19 19:09:17 +0800

    update sql file

--- db_iskotogo.sql
+++ db_iskotogo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2023 at 03:15 AM
+-- Generation Time: Jul 19, 2023 at 01:07 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -42,18 +42,18 @@ CREATE TABLE `tbl_menu` (
 --
 
 INSERT INTO `tbl_menu` (`id`, `store_id`, `item_name`, `item_price`, `inventory`, `item_availability`, `item_image`) VALUES
-(1, 2, 'burgir', 75, 10, 1, ''),
-(2, 1, 'prech price', 55, 9, 1, ''),
-(3, 1, 'spaghetti', 30, 10, 1, ''),
-(4, 1, 'carbonara', 35, 8, 1, ''),
-(5, 1, 'pansit', 40, 10, 1, ''),
+(1, 2, 'burgir', 75, 9, 1, ''),
+(2, 1, 'prech price', 56, 10, 1, ''),
+(3, 1, 'spaghetti', 30, 8, 1, ''),
+(4, 1, 'carbonara', 35, 6, 1, ''),
+(5, 1, 'pansit', 40, 8, 1, ''),
 (6, 1, 'chicken pesto', 69, 10, 1, ''),
 (8, 2, 'coke', 10, 10, 1, ''),
-(9, 2, 'pepsi', 12, 10, 1, ''),
-(10, 2, 'root beer', 15, 10, 1, ''),
+(9, 2, 'pepsi', 12, 9, 1, ''),
+(10, 2, 'root beer', 15, 8, 1, ''),
 (11, 2, 'mountain dew', 17, 10, 1, ''),
 (12, 2, 'sprite', 19, 10, 1, ''),
-(13, 5, 'tokwa', 7, 10, 1, ''),
+(13, 5, 'tokwa', 7, 5, 1, ''),
 (14, 5, 'baboy', 5, 10, 1, ''),
 (15, 5, 'lugaw', 12, 10, 1, ''),
 (16, 5, 'goto', 15, 10, 1, ''),
@@ -63,7 +63,7 @@ INSERT INTO `tbl_menu` (`id`, `store_id`, `item_name`, `item_price`, `inventory`
 (20, 6, 'kwek-kwek', 16, 10, 1, ''),
 (21, 6, 'isaw', 12, 10, 1, ''),
 (22, 6, 'tokneneng', 12, 10, 1, ''),
-(23, 7, 'banana q', 15, 10, 1, ''),
+(23, 7, 'banana q', 15, 8, 1, ''),
 (24, 7, 'camote q', 15, 10, 1, '');
 
 -- --------------------------------------------------------
@@ -91,7 +91,37 @@ INSERT INTO `tbl_orders` (`id`, `item_id`, `store_id`, `customer_id`, `quantity`
 (2, 8, 2, 4, 1, '2023-06-16 02:04:36', 1),
 (3, 13, 5, 12, 5, '2023-06-16 04:55:58', 1),
 (4, 19, 6, 4, 7, '2023-06-16 04:59:08', 0),
-(6, 4, 1, 6, 1, '2023-07-11 22:53:46', 1);
+(8, 2, 1, 6, 1, '2023-07-17 15:13:51', 0),
+(11, 13, 5, 6, 5, '2023-07-17 15:31:54', 0),
+(12, 10, 2, 6, 1, '2023-07-17 22:43:47', 1),
+(13, 9, 2, 6, 1, '2023-07-17 22:44:51', 0),
+(14, 10, 2, 6, 1, '2023-07-19 04:57:12', 0),
+(15, 6, 1, 6, 1, '2023-07-19 07:57:03', 0),
+(16, 4, 1, 6, 2, '2023-07-19 15:54:59', 0),
+(17, 3, 1, 6, 2, '2023-07-19 16:01:20', 0),
+(18, 5, 1, 6, 1, '2023-07-19 17:25:07', 0),
+(19, 5, 1, 6, 1, '2023-07-19 17:49:09', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orderstatus`
+--
+
+CREATE TABLE `tbl_orderstatus` (
+  `id` int(11) NOT NULL,
+  `order_status_id` tinyint(11) DEFAULT NULL,
+  `status` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_orderstatus`
+--
+
+INSERT INTO `tbl_orderstatus` (`id`, `order_status_id`, `status`) VALUES
+(1, 0, 'inactive'),
+(2, 1, 'active'),
+(3, 2, 'for-pickup');
 
 -- --------------------------------------------------------
 
@@ -114,7 +144,7 @@ CREATE TABLE `tbl_stores` (
 --
 
 INSERT INTO `tbl_stores` (`id`, `user_id`, `store_name`, `Location`, `Contact`, `Likes`, `store_image`) VALUES
-(1, 12, 'Jabee', 'PUP Main Lagoon', '', NULL, 'images/users/6-image.jpg'),
+(1, 12, 'Jabee', 'PUP Main Lagoon', '', NULL, ''),
 (2, 7, 'mackdo', 'PUP Main Lagoon', NULL, NULL, NULL),
 (5, 6, 'FFC', 'PUP Main Bldg. Lagoon', NULL, NULL, NULL),
 (6, 8, 'manang isaw', 'PUP Main Bldg. Lagoon', NULL, NULL, NULL),
@@ -134,7 +164,7 @@ CREATE TABLE `tbl_users` (
   `User_Name` varchar(30) NOT NULL,
   `student_id` varchar(15) DEFAULT NULL,
   `user_type` int(1) NOT NULL,
-  `password` int(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
   `section` varchar(9) DEFAULT NULL,
   `user_profile` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -144,12 +174,12 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`user_id`, `Last_Name`, `First_Name`, `Middle_name`, `User_Name`, `student_id`, `user_type`, `password`, `section`, `user_profile`) VALUES
-(4, 'Ledesma', 'Jeriel', 'Esquierdo', '2020-02417-MN-0', '2020-02417-MN-0', 2, 1234, NULL, 'images/users/4-image.jpg'),
-(6, 'Capili', 'Mia Carryl', 'Valentino', '2020-02405-MN-0', '2020-02405-MN-0', 2, 1234, NULL, 'images/users/6-image.jpg'),
-(7, 'Cruz', 'Marielle Nicole', 'Hayag', '2020-02278-MN-0', '2020-02278-MN-0', 2, 1234, NULL, NULL),
-(8, 'Silva', 'Matthew Jericho', 'Pangilinan', '2020-01973-MN-0', '2020-01973-MN-0', 2, 1234, NULL, NULL),
-(11, 'Doe', 'John', NULL, 'Admin', NULL, 1, 1111, NULL, NULL),
-(12, 'Pontillas', 'Harvy', NULL, 'Developer', NULL, 1, 2222, NULL, NULL);
+(4, 'Ledesma', 'Jeriel', 'Esquierdo', '2020-02417-MN-0', '2020-02417-MN-0', 2, '1234', NULL, 'images/users/4-image.jpg'),
+(6, 'Capili', 'Mia Carryl', 'Valentino', '2020-02405-MN-0', '2020-02405-MN-0', 2, 'iskolarngbayan101', 'BSCS 3-4', 'images/users/6-image.jpg'),
+(7, 'Cruz', 'Marielle Nicole', 'Hayag', '2020-02278-MN-0', '2020-02278-MN-0', 2, '1234', NULL, NULL),
+(8, 'Silva', 'Matthew Jericho', 'Pangilinan', '2020-01973-MN-0', '2020-01973-MN-0', 2, '1234', NULL, NULL),
+(11, 'Doe', 'John', NULL, 'Admin', NULL, 3, '1111', NULL, NULL),
+(12, 'Pontillas', 'Harvy', NULL, 'Developer', NULL, 1, '2222', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -189,7 +219,15 @@ ALTER TABLE `tbl_orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `item_id` (`item_id`,`store_id`,`customer_id`),
   ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `store_id` (`store_id`);
+  ADD KEY `store_id` (`store_id`),
+  ADD KEY `order_status` (`order_status`);
+
+--
+-- Indexes for table `tbl_orderstatus`
+--
+ALTER TABLE `tbl_orderstatus`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `order_status_id` (`order_status_id`);
 
 --
 -- Indexes for table `tbl_stores`
@@ -219,13 +257,19 @@ ALTER TABLE `tbl_usertype`
 -- AUTO_INCREMENT for table `tbl_menu`
 --
 ALTER TABLE `tbl_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tbl_orderstatus`
+--
+ALTER TABLE `tbl_orderstatus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_stores`
@@ -255,7 +299,8 @@ ALTER TABLE `tbl_menu`
 ALTER TABLE `tbl_orders`
   ADD CONSTRAINT `tbl_orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `tbl_users` (`user_id`),
   ADD CONSTRAINT `tbl_orders_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `tbl_menu` (`id`),
-  ADD CONSTRAINT `tbl_orders_ibfk_3` FOREIGN KEY (`store_id`) REFERENCES `tbl_stores` (`id`);
+  ADD CONSTRAINT `tbl_orders_ibfk_3` FOREIGN KEY (`store_id`) REFERENCES `tbl_stores` (`id`),
+  ADD CONSTRAINT `tbl_orders_ibfk_4` FOREIGN KEY (`order_status`) REFERENCES `tbl_orderstatus` (`order_status_id`);
 
 --
 -- Constraints for table `tbl_stores`
